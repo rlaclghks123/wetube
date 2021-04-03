7ee42e2da4c906784f7fcdec46a897f7b77912d6
rlaclghks123 <55423198+rlaclghks123@users.noreply.github.com>
2021-04-03 14:36:07 +0900

    problem video time
     it is not working now so I try to fix it

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,6 +3,8 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video")
 const playBtn =document.getElementById("jsPlayButton");
 const volumBtn = document.getElementById("jsVolumButton");
 const fullScrnBtn =document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 function handlePlayClick(){
     if(videoPlayer.paused){
@@ -25,29 +27,13 @@ function handleVolumClick(){
     }
 }
 
+  
+//
 
-function goFullScreen(){
-    if(videoContainer.requestFullScreen){
-        videoContainer.requestFullScreen();
-    }
-    else if(videoContainer.mozCancelRequestFullScreen){
-        videoContainer.mozCancelRequestFullScreen();
-    }
-    else if(videoContainer.webkitRequestFullScreen){
-        videoContainer.webkitRequestFullScreen();
-    }
-    else if(videoContainer.msCancelRequestFullScreen){
-        videoContainer.msRequestFullScreen();
-    }
 
-    fullScrnBtn.innerHTML='<i class="fas fa-compress"></i>';
-    fullScrnBtn.removeEventListener("click",goFullScreen);
-    fullScrnBtn.addEventListener("click",exitFullScreen);
-}
 
 function exitFullScreen(){
     fullScrnBtn.innerHTML='<i class="fas fa-expand"></i>';
-    fullScrnBtn.removeEventListener("click",exitFullScreen);
     fullScrnBtn.addEventListener("click",goFullScreen);
 
     if(document.exitFullscreen){
@@ -63,12 +49,65 @@ function exitFullScreen(){
         document.msExitFullscreen();
     }
 }
+function goFullScreen(){
+    if(videoContainer.requestFullScreen){
+        videoContainer.requestFullScreen();
+    }
+    else if(videoContainer.mozCancelRequestFullScreen){
+        videoContainer.mozCancelRequestFullScreen();
+    }
+    else if(videoContainer.webkitRequestFullScreen){
+        videoContainer.webkitRequestFullScreen();
+    }
+    else if(videoContainer.msCancelRequestFullScreen){
+        videoContainer.msRequestFullScreen();
+    }
+    
+    fullScrnBtn.innerHTML='<i class="fas fa-compress"></i>';
+    fullScrnBtn.removeEventListener("click",goFullScreen);
+    fullScrnBtn.addEventListener("click",exitFullScreen);
+}
+
+
+const formatDate = seconds =>{
+    const secondsNumber = parseInt(seconds,10);
+    let hours = Math.floor(secondsNumber/3600);
+    let minutes = Math.floor((secondsNumber - hours * 3600) /60);
+    let totalSeconds = secondsNumber - hours * 3600 - minutes *60;
+    
+    if(hours<10){
+        hours= `0${hours}`
+    }
+    if(minutes<10){
+        minutes=`0${minutes}`
+    }
+    if(totalSeconds<10){
+        totalSeconds = `0${totalSeconds}`
+    }
+    return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime(){
+    currentTime.innerHTML = formatDate(videoPlayer.currentTime);
+}
+
+
+function setTotalTime(){
+    const totalTimeString = formatDate(videoPlayer.duration);
+totalTime.innerHTML=totalTimeString;
+setInterval(getCurrentTime, 1000);
+}
 
 function init(){
     playBtn.addEventListener("click",handlePlayClick);
     volumBtn.addEventListener("click",handleVolumClick);
     fullScrnBtn.addEventListener("click",goFullScreen);
+    videoPlayer.addEventListener("loadedmetadata", setTotalTime);
+    
+ 
 }
 if(videoContainer){
     init();
 }
+
+
